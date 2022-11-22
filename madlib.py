0a31dd2918075cd9da36861652d4d31e0566aacd
alex-martin0709 <amart6936@yahoo.com>
2022-11-22 00:33:30 -0600

    Creating Python Projects git repo

---
+++ madlib.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+
+
+madlib = f"This madlib beginner project is {adj}. It has really taught me how to {verb1}.\n\
+Hopefully as I progress I will be able to do more {verb2}."
+
+print(madlib)
